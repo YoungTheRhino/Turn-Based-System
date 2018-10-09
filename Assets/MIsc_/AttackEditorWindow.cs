dbe0f865b1f3002ae5041016ea2690b739a195d1
YoungTheRhino <lightning16ry@yahoo.com>
2018-10-08 18:33:40 -0700

    10/8/18

---
+++ Assets/MIsc_/AttackEditorWindow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(EntityScript))]
+[CanEditMultipleObjects]
+public class AttackEditorWindow : Editor {
+
+	public EntityScript entity;
+
+
+	void OnGUI()
+	{
+
+	}
+}
---
+++ Assets/Managers/ItemManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public abstract class Item : MonoBehaviour {
+
+    public Image image;
+
+    public int dictIndex;
+
+    public void Use()
+    {
+        
+    }
+}
+
+public class ItemManager : MonoBehaviour {
+
+    public List<Item> publicInventory;
+    public static Dictionary<int, Item> itemDictionary;
+
+    public Dictionary<int, int> currInventory;
+
+	// Use this for initialization
+	void Start () {
+        SetDictionary();
+	}
+	
+    public void SetDictionary()
+    {
+        foreach(Item i in publicInventory)
+        {
+            itemDictionary.Add(i.dictIndex, i);
+        }
+        print(itemDictionary);
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
